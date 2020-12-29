c6c8c7601a4bd3ca114208998a2a0ecc11408b7b
Krzysztof Smigielski <nil.caream@gmail.com>
2020-12-29 21:59:39 +0100

    Initial commit of Spring Initializr.

---
+++ src/main/java/com/nilcaream/werewolves/WerewolvesApplication.java
@@ -0,0 +1,13 @@
+package com.nilcaream.werewolves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WerewolvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WerewolvesApplication.class, args);
+	}
+
+}
