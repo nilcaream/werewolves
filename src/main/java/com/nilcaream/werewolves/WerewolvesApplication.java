d8c5cf9d07a88171365cd952e6c52b2f54c39082
Krzysztof Smigielski <nil.caream@gmail.com>
2020-12-29 22:04:03 +0100

    Updated pom and reformatted auto-generated files.

--- src/main/java/com/nilcaream/werewolves/WerewolvesApplication.java
+++ src/main/java/com/nilcaream/werewolves/WerewolvesApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WerewolvesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WerewolvesApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WerewolvesApplication.class, args);
+    }
 }
