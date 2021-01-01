43bc5447a129002f3b7bee7dfc9002e01ad660f7
Krzysztof Smigielski <nil.caream@gmail.com>
2021-01-01 23:07:28 +0100

    Added test cases. Removed old code.

--- src/test/java/com/nilcaream/werewolves/FlowTest.java
+++ src/test/java/com/nilcaream/werewolves/FlowTest.java
@@ -5,13 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static java.util.Collections.emptyList;
 import static java.util.List.of;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -42,27 +40,20 @@ class FlowTest {
     @Test
     void shouldExecuteGame1a() {
         // given
-        Game game = underTest.createGame("game1", of(Role.WEREWOLF, Role.WEREWOLF, Role.SEER, Role.ROBBER, Role.TROUBLEMAKER, Role.VILLAGER));
-        underTest.createPlayer("game1", "p1", "Player1");
-        underTest.createPlayer("game1", "p2", "Player2");
-        underTest.createPlayer("game1", "p3", "Player3");
-
-        // roles setup - c0, c1, c2, p1, p2, p3
-        // state: robber, troublemaker, villager. center: werewolf, werewolf, seer
-        startGame(game, Role.WEREWOLF, Role.WEREWOLF, Role.SEER, Role.ROBBER, Role.TROUBLEMAKER, Role.VILLAGER);
+        Game game = createGame(of(Role.ROBBER, Role.TROUBLEMAKER, Role.VILLAGER), of(Role.WEREWOLF, Role.WEREWOLF, Role.SEER));
 
         // when
-        Player.State state1 = underTest.executeAction("game1", "p1", of("p3"));// state: villager, troublemaker, robber. center: werewolf, werewolf, seer
-        Player.State state2 = underTest.executeAction("game1", "p2", of("p1", "p3"));// state: robber, troublemaker, villager. center: werewolf, werewolf, seer
-        Player.State state3 = underTest.executeAction("game1", "p3", emptyList());// state: robber, troublemaker, villager. center: werewolf, werewolf, seer
+        Player.State state1 = underTest.executeAction("game1", "p0", of("p2"));
+        Player.State state2 = underTest.executeAction("game1", "p1", of("p0", "p2"));
+        Player.State state3 = underTest.executeAction("game1", "p2", of());
 
         // then
         assertThat(state1).isEqualTo(Player.State.WORKING);
         assertThat(state2).isEqualTo(Player.State.WORKING);
         assertThat(state3).isEqualTo(Player.State.READY_TO_PLAY);
-        assertState(underTest.playerStatus("game1", "p1"), Player.State.READY_TO_PLAY, "p3", Role.ROBBER, "p1", Role.VILLAGER);
-        assertState(underTest.playerStatus("game1", "p2"), Player.State.READY_TO_PLAY);
-        assertState(underTest.playerStatus("game1", "p3"), Player.State.READY_TO_PLAY);
+        assertState(game, "p0", Role.ROBBER, "p0", Role.VILLAGER, "p2", Role.ROBBER);
+        assertState(game, "p1", Role.TROUBLEMAKER);
+        assertState(game, "p2", Role.VILLAGER);
     }
 
     // robber, troublemaker, villager. Robber takes villager role. Troublemaker reverts robber move
@@ -70,27 +61,20 @@ class FlowTest {
     @Test
     void shouldExecuteGame1b() {
         // given
-        Game game = underTest.createGame("game1", of(Role.WEREWOLF, Role.WEREWOLF, Role.SEER, Role.ROBBER, Role.TROUBLEMAKER, Role.VILLAGER));
-        underTest.createPlayer("game1", "p1", "Player1");
-        underTest.createPlayer("game1", "p2", "Player2");
-        underTest.createPlayer("game1", "p3", "Player3");
-
-        // roles setup - c0, c1, c2, p1, p2, p3
-        // state: robber, troublemaker, villager. center: werewolf, werewolf, seer
-        startGame(game, Role.WEREWOLF, Role.WEREWOLF, Role.SEER, Role.ROBBER, Role.TROUBLEMAKER, Role.VILLAGER);
+        Game game = createGame(of(Role.ROBBER, Role.TROUBLEMAKER, Role.VILLAGER), of(Role.WEREWOLF, Role.WEREWOLF, Role.SEER));
 
         // when
-        Player.State state2 = underTest.executeAction("game1", "p2", of("p1", "p3"));
-        Player.State state3 = underTest.executeAction("game1", "p3", emptyList());
-        Player.State state1 = underTest.executeAction("game1", "p1", of("p3"));
+        Player.State state2 = underTest.executeAction("game1", "p1", of("p0", "p2"));
+        Player.State state3 = underTest.executeAction("game1", "p2", of());
+        Player.State state1 = underTest.executeAction("game1", "p0", of("p2"));
 
         // then
         assertThat(state2).isEqualTo(Player.State.WORKING);
         assertThat(state3).isEqualTo(Player.State.WORKING);
         assertThat(state1).isEqualTo(Player.State.READY_TO_PLAY);
-        assertState(underTest.playerStatus("game1", "p1"), Player.State.READY_TO_PLAY, "p3", Role.ROBBER, "p1", Role.VILLAGER);
-        assertState(underTest.playerStatus("game1", "p2"), Player.State.READY_TO_PLAY);
-        assertState(underTest.playerStatus("game1", "p3"), Player.State.READY_TO_PLAY);
+        assertState(game, "p0", Role.ROBBER, "p0", Role.VILLAGER, "p2", Role.ROBBER);
+        assertState(game, "p1", Role.TROUBLEMAKER);
+        assertState(game, "p2", Role.VILLAGER);
     }
 
     @Test
@@ -101,7 +85,7 @@ class FlowTest {
         // when
         underTest.executeAction("game1", "p0", of("p3")); // robber. p0 werewolf. p3 robber
         underTest.executeAction("game1", "p1", of("p0", "p2")); // troublemaker. p0 villager. p2 werewolf
-        underTest.executeAction("game1", "p2", emptyList()); // villager
+        underTest.executeAction("game1", "p2", of()); // villager
         underTest.executeAction("game1", "p3", of("Center2")); // werewolf
 
         // then
@@ -135,10 +119,10 @@ class FlowTest {
         Game game = createGame(of(Role.WEREWOLF, Role.WEREWOLF, Role.VILLAGER, Role.VILLAGER), of(Role.SEER, Role.TROUBLEMAKER, Role.ROBBER));
 
         // when
-        underTest.executeAction("game1", "p0", emptyList());
-        underTest.executeAction("game1", "p1", emptyList());
-        underTest.executeAction("game1", "p2", emptyList());
-        underTest.executeAction("game1", "p3", emptyList());
+        underTest.executeAction("game1", "p0", of());
+        underTest.executeAction("game1", "p1", of());
+        underTest.executeAction("game1", "p2", of());
+        underTest.executeAction("game1", "p3", of());
 
         // then
         assertState(game, "p0", Role.WEREWOLF, "p1", Role.WEREWOLF);
@@ -147,16 +131,22 @@ class FlowTest {
         assertState(game, "p3", Role.VILLAGER);
     }
 
-    private void startGame(Game game, Role... roles) {
-        underTest.startGame("game1");
-        List<Player> players = game.getPlayers().stream().sorted(Comparator.comparing(Player::getId)).collect(Collectors.toList());
-        assertThat(roles.length).isEqualTo(players.size());
-        for (int i = 0, rolesLength = roles.length; i < rolesLength; i++) {
-            Player player = players.get(i);
-            player.getRoles().clear();
-            player.getRoles().add(roles[i]);
-            logger.info("Player {} as {}", player.getId(), roles[i]);
-        }
+    @Test
+    void shouldExecuteGame5a() {
+        // given
+        Game game = createGame(of(Role.WEREWOLF, Role.WEREWOLF, Role.SEER, Role.ROBBER), of(Role.VILLAGER, Role.TROUBLEMAKER, Role.VILLAGER));
+
+        // when
+        underTest.executeAction("game1", "p0", of());
+        underTest.executeAction("game1", "p1", of());
+        underTest.executeAction("game1", "p2", of("p1"));
+        underTest.executeAction("game1", "p3", of("p1"));
+
+        // then
+        assertState(game, "p0", Role.WEREWOLF, "p1", Role.WEREWOLF);
+        assertState(game, "p1", Role.ROBBER, "p0", Role.WEREWOLF);
+        assertState(game, "p2", Role.SEER, "p1", Role.WEREWOLF);
+        assertState(game, "p3", Role.WEREWOLF, "p1", Role.ROBBER, "p3", Role.WEREWOLF);
     }
 
     private Game createGame(List<Role> playerRoles, List<Role> centerRoles) {
@@ -224,35 +214,6 @@ class FlowTest {
         assertThat(status.getPlayers().get(pId2)).isEqualByComparingTo(expectedRole2);
 
         Set<Role> otherRoles = status.getPlayers().entrySet().stream()
-                .filter(e -> !e.getKey().equals(pId1))
-                .filter(e -> !e.getKey().equals(pId2))
-                .map(Map.Entry::getValue)
-                .collect(Collectors.toSet());
-        assertThat(otherRoles).containsOnly(Role.UNKNOWN);
-    }
-
-    private void assertState(Flow.PlayerStatus actual, Player.State expectedState) {
-        assertThat(actual.getStatus()).isEqualByComparingTo(expectedState);
-        assertThat(actual.getPlayers().values()).containsOnly(Role.UNKNOWN);
-    }
-
-    private void assertState(Flow.PlayerStatus actual, Player.State expectedState, String pId1, Role expectedRole1) {
-        assertThat(actual.getStatus()).isEqualByComparingTo(expectedState);
-        assertThat(actual.getPlayers().get(pId1)).isEqualByComparingTo(expectedRole1);
-
-        Set<Role> otherRoles = actual.getPlayers().entrySet().stream()
-                .filter(e -> !e.getKey().equals(pId1))
-                .map(Map.Entry::getValue)
-                .collect(Collectors.toSet());
-        assertThat(otherRoles).containsOnly(Role.UNKNOWN);
-    }
-
-    private void assertState(Flow.PlayerStatus actual, Player.State expectedState, String pId1, Role expectedRole1, String pId2, Role expectedRole2) {
-        assertThat(actual.getStatus()).isEqualByComparingTo(expectedState);
-        assertThat(actual.getPlayers().get(pId1)).isEqualByComparingTo(expectedRole1);
-        assertThat(actual.getPlayers().get(pId2)).isEqualByComparingTo(expectedRole2);
-
-        Set<Role> otherRoles = actual.getPlayers().entrySet().stream()
                 .filter(e -> !e.getKey().equals(pId1))
                 .filter(e -> !e.getKey().equals(pId2))
                 .map(Map.Entry::getValue)
